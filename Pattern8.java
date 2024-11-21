4581747dfb4edb41bc1460e253260d222c513b80
Patel-Tripti <139382407+Patel-Tripti@users.noreply.github.com>
2024-11-21 10:51:18 +0530

    Add files via upload

---
+++ Pattern8.java
@@ -0,0 +1,18 @@
+package GroupAssignment;
+
+public class Pattern8 {
+    public static void main(String[] args) {
+        int count =0;
+        for(int i=1;i<=4;i++){
+        for(int j = 1;j<=i;j++){
+         count ++;
+         System.out.print(count);
+        }
+        System.out.println("");
+    }
+
+}}
+// 1
+// 23
+// 456
+// 78910
